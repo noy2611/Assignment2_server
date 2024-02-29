7133fb007d841008849fefc93600b0af36a9b659
noy2611 <noyr2611@gmail.com>
2024-02-29 10:22:45 +0200

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
+app.use(cors());
 
 const { planRouter } = require("./router/plan.router");
 
